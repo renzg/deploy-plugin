ad63aabe3efd683fc81f9eadc9981f3e84d023e5
rzg <renzenggang@gmail.com>
2017-04-12 16:26:38 +0800

    merge
    merge

--- src/main/java/hudson/plugins/deploy/CargoContainerAdapter.java
+++ src/main/java/hudson/plugins/deploy/CargoContainerAdapter.java
@@ -1,15 +1,5 @@
 package hudson.plugins.deploy;
 
-import hudson.EnvVars;
-import hudson.FilePath;
-import hudson.FilePath.FileCallable;
-import hudson.Launcher;
-import hudson.Util;
-import hudson.model.BuildListener;
-import hudson.model.AbstractBuild;
-import hudson.remoting.VirtualChannel;
-import hudson.util.VariableResolver;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
@@ -30,6 +20,16 @@ import org.codehaus.cargo.generic.configuration.DefaultConfigurationFactory;
 import org.codehaus.cargo.generic.deployer.DefaultDeployerFactory;
 import org.codehaus.cargo.generic.deployer.DeployerFactory;
 
+import hudson.EnvVars;
+import hudson.FilePath;
+import hudson.FilePath.FileCallable;
+import hudson.Launcher;
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
+import hudson.util.VariableResolver;
+
 /**
  * Provides container-specific glue code.
  *
@@ -41,97 +41,108 @@ import org.codehaus.cargo.generic.deployer.DeployerFactory;
  */
 public abstract class CargoContainerAdapter extends ContainerAdapter implements Serializable {
 
-    /**
-     * Returns the container ID used by Cargo.
-     *
-     * @return
-     */
-    protected abstract String getContainerId();
+	/**
+	 * Returns the container ID used by Cargo.
+	 *
+	 * @return
+	 */
+	protected abstract String getContainerId();
 
-    /**
-     * Fills in the {@link Configuration} object.
-     *
-     * @param config
-     */
-    protected abstract void configure(Configuration config, EnvVars envVars, VariableResolver<String> resolver);
+	/**
+	 * Fills in the {@link Configuration} object.
+	 *
+	 * @param config
+	 */
+	protected abstract void configure(Configuration config, EnvVars envVars, VariableResolver<String> resolver);
 
-    protected Container getContainer(ConfigurationFactory configFactory, ContainerFactory containerFactory, String id, EnvVars envVars, VariableResolver<String> resolver) {
-        Configuration config = configFactory.createConfiguration(id, ContainerType.REMOTE, ConfigurationType.RUNTIME);
-        configure(config, envVars, resolver);
-        return containerFactory.createContainer(id, ContainerType.REMOTE, config);
-    }
+	protected Container getContainer(ConfigurationFactory configFactory, ContainerFactory containerFactory, String id,
+			EnvVars envVars, VariableResolver<String> resolver) {
+		Configuration config = configFactory.createConfiguration(id, ContainerType.REMOTE, ConfigurationType.RUNTIME);
 
-    protected void deploy(DeployerFactory deployerFactory, final BuildListener listener, Container container, File f, String contextPath) {
-        Deployer deployer = deployerFactory.createDeployer(container);
+		System.out.println(String.format("部署配置：%s", String.valueOf(config.getProperties())));
 
-        listener.getLogger().println("Deploying " + f + " to container " + container.getName() + " with context " + contextPath);
-        deployer.setLogger(new LoggerImpl(listener.getLogger()));
+		configure(config, envVars, resolver);
 
+		Container container = containerFactory.createContainer(id, ContainerType.REMOTE, config);
+		System.out.println("部署目标容器:" + container.getName() + "," + container.getType());
+		return container;
+	}
 
-        String extension = FilenameUtils.getExtension(f.getAbsolutePath());
-        if ("WAR".equalsIgnoreCase(extension)) {
-            WAR war = createWAR(f);
-            if (!StringUtils.isEmpty(contextPath)) {
-                war.setContext(contextPath);
-            }
-            deployer.redeploy(war);
-        } else if ("EAR".equalsIgnoreCase(extension)) {
-            EAR ear = createEAR(f);
-            deployer.redeploy(ear);
-        } else {
-            throw new RuntimeException("Extension File Error.");
-        }
-    }
+	protected void deploy(DeployerFactory deployerFactory, final BuildListener listener, Container container, File f,
+			String contextPath) {
+		Deployer deployer = deployerFactory.createDeployer(container);
 
-    /**
-     * Creates a Deployable object WAR from the given file object.
-     *
-     * @param deployableFile The deployable file to create the Deployable from.
-     * @return A Deployable object.
-     */
-    protected WAR createWAR(File deployableFile) {
-        return new WAR(deployableFile.getAbsolutePath());
-    }
+		listener.getLogger().println("Deploying " + f + " to container( " + deployer.toString() + ")"
+				+ container.getName() + " with context " + contextPath);
+		deployer.setLogger(new LoggerImpl(listener.getLogger()));
 
-    protected String expandVariable(EnvVars envVars, VariableResolver<String> resolver, String variable) {
-        String temp = envVars.expand(variable);
-        return Util.replaceMacro(envVars.expand(variable), resolver);
-    }
+		String extension = FilenameUtils.getExtension(f.getAbsolutePath());
+		if ("WAR".equalsIgnoreCase(extension)) {
+			WAR war = createWAR(f);
+			if (!StringUtils.isEmpty(contextPath)) {
+				war.setContext(contextPath);
+			}
+			deployer.redeploy(war);
+		} else if ("EAR".equalsIgnoreCase(extension)) {
+			EAR ear = createEAR(f);
+			deployer.redeploy(ear);
+		} else {
+			throw new RuntimeException("Extension File Error.");
+		}
+	}
 
+	/**
+	 * Creates a Deployable object WAR from the given file object.
+	 *
+	 * @param deployableFile
+	 *            The deployable file to create the Deployable from.
+	 * @return A Deployable object.
+	 */
+	protected WAR createWAR(File deployableFile) {
+		return new WAR(deployableFile.getAbsolutePath());
+	}
 
-    /**
-     * Creates a Deployable object EAR from the given file object.
-     *
-     * @param deployableFile The deployable file to create the Deployable from.
-     * @return A Deployable object.
-     */
-    protected EAR createEAR(File deployableFile) {
-        return new EAR(deployableFile.getAbsolutePath());
-    }
+	protected String expandVariable(EnvVars envVars, VariableResolver<String> resolver, String variable) {
+		String temp = envVars.expand(variable);
+		return Util.replaceMacro(envVars.expand(variable), resolver);
+	}
 
-    public boolean redeploy(FilePath war, final String contextPath, final AbstractBuild<?, ?> build, Launcher launcher, final BuildListener listener) throws IOException, InterruptedException {
-        return war.act(new FileCallable<Boolean>() {
-            public Boolean invoke(File f, VirtualChannel channel) throws IOException {
-                if (!f.exists()) {
-                    listener.error(Messages.DeployPublisher_NoSuchFile(f));
-                    return true;
-                }
-                ClassLoader cl = getClass().getClassLoader();
-                final ConfigurationFactory configFactory = new DefaultConfigurationFactory(cl);
-                final ContainerFactory containerFactory = new DefaultContainerFactory(cl);
-                final DeployerFactory deployerFactory = new DefaultDeployerFactory(cl);
+	/**
+	 * Creates a Deployable object EAR from the given file object.
+	 *
+	 * @param deployableFile
+	 *            The deployable file to create the Deployable from.
+	 * @return A Deployable object.
+	 */
+	protected EAR createEAR(File deployableFile) {
+		return new EAR(deployableFile.getAbsolutePath());
+	}
 
-                try {
-                    final EnvVars envVars = build.getEnvironment(listener);
-                    final VariableResolver<String> resolver = build.getBuildVariableResolver();
-                    Container container = getContainer(configFactory, containerFactory, getContainerId(), envVars, resolver);
-                    deploy(deployerFactory, listener, container, f, expandVariable(envVars, resolver, contextPath));
-                } catch (InterruptedException e) {
-                    throw new RuntimeException("Failed to get build environment", e);
-            	}
+	public boolean redeploy(FilePath war, final String contextPath, final AbstractBuild<?, ?> build, Launcher launcher,
+			final BuildListener listener) throws IOException, InterruptedException {
+		return war.act(new FileCallable<Boolean>() {
+			public Boolean invoke(File f, VirtualChannel channel) throws IOException {
+				if (!f.exists()) {
+					listener.error(Messages.DeployPublisher_NoSuchFile(f));
+					return true;
+				}
+				ClassLoader cl = getClass().getClassLoader();
+				final ConfigurationFactory configFactory = new DefaultConfigurationFactory(cl);
+				final ContainerFactory containerFactory = new DefaultContainerFactory(cl);
+				final DeployerFactory deployerFactory = new DefaultDeployerFactory(cl);
 
-                return true;
-            }
-        });
-    }
+				try {
+					final EnvVars envVars = build.getEnvironment(listener);
+					final VariableResolver<String> resolver = build.getBuildVariableResolver();
+					Container container = getContainer(configFactory, containerFactory, getContainerId(), envVars,
+							resolver);
+					deploy(deployerFactory, listener, container, f, expandVariable(envVars, resolver, contextPath));
+				} catch (InterruptedException e) {
+					throw new RuntimeException("Failed to get build environment", e);
+				}
+
+				return true;
+			}
+		});
+	}
 }
--- src/main/java/hudson/plugins/deploy/DeployPublisher.java
+++ src/main/java/hudson/plugins/deploy/DeployPublisher.java
@@ -27,54 +27,57 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * @author Kohsuke Kawaguchi
  */
 public class DeployPublisher extends Notifier implements Serializable {
-    private List<ContainerAdapter> adapters;
-    public final String contextPath;
+	private List<ContainerAdapter> adapters;
+	public final String contextPath;
 
-    public final String war;
-    public final boolean onFailure;
+	public final String war;
+	public final boolean onFailure;
 
-    /**
-     * @deprecated
-     *      Use {@link #getAdapters()}
-     */
-    public final ContainerAdapter adapter = null;
-    
-    @DataBoundConstructor
-    public DeployPublisher(List<ContainerAdapter> adapters, String war, String contextPath, boolean onFailure) {
-   		this.adapters = adapters;
-        this.war = war;
-        this.onFailure = onFailure;
-        this.contextPath = contextPath;
-    }
+	/**
+	 * @deprecated Use {@link #getAdapters()}
+	 */
+	public final ContainerAdapter adapter = null;
 
-    @Override
-    public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-        if (build.getResult().equals(Result.SUCCESS) || onFailure) {
-            for (FilePath warFile : build.getWorkspace().list(this.war)) {
-                for (ContainerAdapter adapter : adapters)
-                    if (!adapter.redeploy(warFile, contextPath, build, launcher, listener))
-                        build.setResult(Result.FAILURE);
-            }
-        }
+	@DataBoundConstructor
+	public DeployPublisher(List<ContainerAdapter> adapters, String war, String contextPath, boolean onFailure) {
+		this.adapters = adapters;
+		this.war = war;
+		this.onFailure = onFailure;
+		this.contextPath = contextPath;
+	}
 
-        return true;
-    }
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+			throws InterruptedException, IOException {
+		if (build.getResult().equals(Result.SUCCESS) || onFailure) {
+			for (FilePath warFile : build.getWorkspace().list(this.war)) {
+				for (ContainerAdapter adapter : adapters) {
+					listener.getLogger().println("准备部署：" + warFile.getName() + " 到容器,上下文:" + contextPath);
+					if (!adapter.redeploy(warFile, contextPath, build, launcher, listener)) {
+						build.setResult(Result.FAILURE);
+					}
+				}
+			}
+		}
 
-    public BuildStepMonitor getRequiredMonitorService() {
-        return BuildStepMonitor.BUILD;
-    }
-    
-    public Object readResolve() {
-    	if(adapter != null) {
-    		if(adapters == null) {
-    			adapters = new ArrayList<ContainerAdapter>();
-    		}
-    		adapters.add(adapter);
-    	}
-    	return this;
-    }
+		return true;
+	}
 
-    /**
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
+
+	public Object readResolve() {
+		if (adapter != null) {
+			if (adapters == null) {
+				adapters = new ArrayList<ContainerAdapter>();
+			}
+			adapters.add(adapter);
+		}
+		return this;
+	}
+
+	/**
 	 * Get the value of the adapterWrappers property
 	 *
 	 * @return The value of adapterWrappers
@@ -84,28 +87,29 @@ public class DeployPublisher extends Notifier implements Serializable {
 	}
 
 	@Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
-        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-            return true;
-        }
+	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return true;
+		}
 
-        public String getDisplayName() {
-            return Messages.DeployPublisher_DisplayName();
-        }
+		public String getDisplayName() {
+			return Messages.DeployPublisher_DisplayName();
+		}
 
-        /**
-         * Sort the descriptors so that the order they are displayed is more predictable
-         */
-        public List<ContainerAdapterDescriptor> getAdaptersDescriptors() {
-            List<ContainerAdapterDescriptor> r = new ArrayList<ContainerAdapterDescriptor>(ContainerAdapter.all());
-            Collections.sort(r,new Comparator<ContainerAdapterDescriptor>() {
-                public int compare(ContainerAdapterDescriptor o1, ContainerAdapterDescriptor o2) {
-                    return o1.getDisplayName().compareTo(o2.getDisplayName());
-                }
-            });
-            return r;
-        }
-    }
+		/**
+		 * Sort the descriptors so that the order they are displayed is more
+		 * predictable
+		 */
+		public List<ContainerAdapterDescriptor> getAdaptersDescriptors() {
+			List<ContainerAdapterDescriptor> r = new ArrayList<ContainerAdapterDescriptor>(ContainerAdapter.all());
+			Collections.sort(r, new Comparator<ContainerAdapterDescriptor>() {
+				public int compare(ContainerAdapterDescriptor o1, ContainerAdapterDescriptor o2) {
+					return o1.getDisplayName().compareTo(o2.getDisplayName());
+				}
+			});
+			return r;
+		}
+	}
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 }
